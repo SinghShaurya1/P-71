abc40407327da6ffc53a58b54a6a160441a5e38d
SinghShaurya1 <104992391+SinghShaurya1@users.noreply.github.com>
2023-03-27 19:37:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,15 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 //Paste your firebaseConfig here
+const firebaseConfig = {
+  apiKey: "AIzaSyCGyLM--nz27FQaNNNOgjiprXbEAkqfo7Y",
+  authDomain: "e-bike-299a5.firebaseapp.com",
+  projectId: "e-bike-299a5",
+  storageBucket: "e-bike-299a5.appspot.com",
+  messagingSenderId: "482964724727",
+  appId: "1:482964724727:web:01b80703eff635b8ccacf2"
+};
 
-//
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.firestore();
